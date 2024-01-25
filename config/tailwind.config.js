b41872e9a58d222752ba06ac6d1f848dbe308476
Felipe Brasil <felipesbrsl@gmail.com>
2024-01-26 00:44:06 +0100

    init

---
+++ config/tailwind.config.js
@@ -0,0 +1,27 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+module.exports = {
+  content: [
+    './public/*.html',
+    './app/helpers/**/*.rb',
+    './app/javascript/**/*.js',
+    './app/views/**/*.{erb,haml,html,slim}'
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
+      colors:{
+        "isu-green": "#094927",
+        "isu-yellow": "#FFAB0E",
+      },
+    },
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/aspect-ratio'),
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/container-queries'),
+  ]
+}
